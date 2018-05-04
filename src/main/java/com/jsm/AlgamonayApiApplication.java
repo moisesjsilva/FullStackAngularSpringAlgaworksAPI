ae38c6804f1f00c87c1a4cdf7912a3e1df2fa5d7
developmentpdd <development.pdd@gmail.com>
2018-05-04 09:49:54 -0300

    Primeiro Commit

---
+++ src/main/java/com/jsm/AlgamonayApiApplication.java
@@ -0,0 +1,12 @@
+package com.jsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamonayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamonayApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jsm/AlgamonayApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jsm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgamonayApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
